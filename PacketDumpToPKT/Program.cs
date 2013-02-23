5fc6076ef7cf20ee6be52bd87d828ebe0f7f128a
elecyb <elecyb@hotmail.com>
2013-02-22 22:57:35 -0300

    Initial import

---
+++ PacketDumpToPKT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace PacketDumpToPKT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get all PacketDump.xxxxxx files from current directory 
+            string[] filePaths = Array.FindAll(Directory.GetFiles(Directory.GetCurrentDirectory(), "PacketDump.*"), x => !x.EndsWith(".pkt"));
+
+            foreach (var filePath in filePaths)
+            {
+                Console.WriteLine("Parsing {0} ...", Path.GetFileName(filePath));
+                Parser.ParseFile(filePath);
+            }
+
+            Console.WriteLine("\n Parsed all files, press a key to exit...");
+            Console.ReadLine();
+
+        }
+    }
+}
